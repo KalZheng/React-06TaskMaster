44dbb3cc56cbc04abc1b4806204cb97bc4b68824
Kal Zheng <kal.zheng@inspire-dt.com>
2023-12-05 15:22:09 +0800

    button function to switch between no project or add project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    SelectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        SelectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+  if (projectState.SelectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.SelectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      {/* <NewProject /> */}
-      <NoProjectSelected />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
